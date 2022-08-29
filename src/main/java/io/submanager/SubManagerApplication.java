1cd0965557ea629efe4b1e55814b3ec17b4f63b9
Lucas Falabretti <lucasfalabretti7@gmail.com>
2022-08-28 23:32:20 -0300

    Create base project

---
+++ src/main/java/io/submanager/SubManagerApplication.java
@@ -0,0 +1,14 @@
+package io.submanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class SubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/io/submanager/SubmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.submanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
